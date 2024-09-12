903beb984be1d79bab26534bb107ece95c595ee0
MSEdgeDev <113806044+MSEdgeDev@users.noreply.github.com>
2024-09-12 11:47:12 +0800

    Add files via upload

---
+++ Define/BasicDefine.h
@@ -0,0 +1,8 @@
+#define MAX_ARRAY_SIZE 255
+
+#define CONFIG_FILENAME "Config.ini"
+#define LOG_FILENAME "ErrorReportLog.log"
+
+#define MEMORY_ERROR 1
+
+#define DEFAULT_ERROR MEMORY_ERROR
---
+++ ErrorReportMain.c
@@ -0,0 +1,26 @@
+#include <Windows.h>
+#include <stdio.h>
+
+#include "Define/BasicDefine.h"
+#include "AutoMemoryError/AutoMemoryError.h"
+
+int ErrorType = DEFAULT_ERROR;
+SYSTEMTIME MainStartTime;
+
+int main() {
+	FILE* ConfigFileReader = fopen(CONFIG_FILENAME, "r");
+	FILE* LogWriter = fopen(LOG_FILENAME, "at+");
+	
+	GetLocalTime(&MainStartTime);
+	
+	fscanf(ConfigFileReader, "%d", &ErrorType);
+	
+	switch (ErrorType) {
+		case 1: {
+			MemoryErrorMain(MainStartTime, ConfigFileReader, LogWriter);
+			break;
+		}
+	}
+	
+	return 0;
+}
