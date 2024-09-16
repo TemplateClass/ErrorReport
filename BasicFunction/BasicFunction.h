9b3d8c2984595fe8bb6f3296aa8217de0851579e
TemplateClass <113806044+TemplateClass@users.noreply.github.com>
2024-09-16 12:50:59 +0800

    Delete BasicFunction directory

--- BasicFunction/BasicFunction.h
+++
@@ -1,6 +0,0 @@
-void GetFormatTime(SYSTEMTIME Time, char* TimeString) {
-	sprintf(TimeString, "%04d/%02d/%02d %02d:%02d:%02d.%03d",
-		Time.wYear, Time.wMonth, Time.wDay, Time.wHour, 
-		Time.wMinute, Time.wSecond, Time.wMilliseconds
-	);
-} 
